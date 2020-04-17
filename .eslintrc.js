c16fec4ed130ca90c3243df236a7ec1a67c1652c
Raclett3 <im@s283.net>
2020-04-17 16:23:08 +0900

    Ignore unused vars that start with underscore

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     'import/default': 'off',
