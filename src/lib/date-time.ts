c22a67705237997a102351bc17d17fa682f8f946
Raclett3 <im@s283.net>
2020-05-02 14:10:20 +0900

    Fix dateTime()

--- src/lib/date-time.ts
+++ src/lib/date-time.ts
@@ -1,5 +1,5 @@
 export function dateTime(date: Date) {
-  const dateString = `${date.getFullYear()}/${date.getMonth()}/${date.getDate()}`;
+  const dateString = `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
   const timeString = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
   return `${dateString} ${timeString}`;
 }
