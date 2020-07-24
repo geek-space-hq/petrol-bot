da9e10cedfeaf574517ad8ab19223c66af4b542c
aiotter <git@aiotter.com>
2020-07-24 23:39:31 +0900

    Format code

--- src/modules/vim-help.ts
+++ src/modules/vim-help.ts
@@ -1,9 +1,10 @@
 import Discord from 'discord.js';
+
 import { NextFunction } from '../bot';
 
-const { VimHelp } = require("vimhelp");
-let vimHelp = new VimHelp();
-vimHelp.helplang = ['ja', 'en']  // if Japanese help is installed, use it
+const { VimHelp } = require('vimhelp');
+const vimHelp = new VimHelp();
+vimHelp.helplang = ['ja', 'en']; // if Japanese help is installed, use it
 
 export async function searchVimHelp(message: Discord.Message, client: Discord.Client, next: NextFunction) {
   if (!message.guild || !client.user || message.author.id === client.user.id) {
@@ -18,7 +19,12 @@ export async function searchVimHelp(message: Discord.Message, client: Discord.Cl
   }
 
   try {
-    const helpText = await vimHelp.search(content.split(' ').slice(1).join(' '));
+    const helpText = await vimHelp.search(
+      content
+        .split(' ')
+        .slice(1)
+        .join(' '),
+    );
     const sliced = helpText.length > 1900 ? helpText.slice(0, 1900) + '...' : helpText;
     const sending = `\`\`\`\n${sliced}\n\`\`\``;
     await message.channel.send(sending);
